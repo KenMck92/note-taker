eebf9cdce42aadfa05e06644be8179a161bcc2eb
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-08-01 20:42:16 -0500

    added server.js and other files needed for challenge

---
+++ server.js
@@ -0,0 +1,15 @@
+// Import necessary js files
+
+// create port and app
+
+// Use middleware
+
+// GET route for homepage
+
+// Get route for notes page
+
+// GET route for API
+
+// POST route for API
+
+// listen() method
